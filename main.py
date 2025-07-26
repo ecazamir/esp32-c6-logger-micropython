babd7a90e9d450a19ba4e323ed9de87f31d4472e
Emil Cazamir <77853553+ecazamir@users.noreply.github.com>
2025-07-26 18:17:55 +0300

    Switched timer to lightsleep, added blink, enforced fsync on log data, made timing variables config params

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, os, machine, utime
+import sys, os, machine, utime, time
 
 ctime = rtc.datetime()
 print(
@@ -32,16 +32,19 @@ def log_data(log_file_name="", payload="log payload not set"):
         # log_file_name = get_log_file_name()
         # TODO: populate the log entry here
         # Format log entry
-        log_entry = f'"{timestamp}","{payload}"'
+        log_entry = f'"{timestamp}",{payload}'
 
         # Write to the file
         with open(log_file_name, "a") as log_file:
             log_file.write(log_entry + "\n")
-            print(f"Logged successfully: {log_file_name}: {log_entry}")
+            if (config.DEBUG_MODE):
+                print(f"Logged successfully: {log_file_name}: {log_entry}")
             log_file.close()
+        os.sync()
 
     except Exception as e:
         print("log_data: An error occurred acquiring data or saving it to the log:", e)
+        sys.exit(0)
 
     logging_platform.set_neopixel_rgb(0, 0, 0)
 
@@ -49,18 +52,24 @@ def log_data(log_file_name="", payload="log payload not set"):
 ####
 # main code
 # print(f"Machine type: {MACHINE_Type}")
-print("Sleeping 10s to allow program termination / update")
-utime.sleep(10)
+print(f"Sleeping {config.START_MAIN_DELAY_SECONDS}s to allow program termination / update")
+utime.sleep(config.START_MAIN_DELAY_SECONDS)
 print("Entering main loop")
+start_time_ms = time.ticks_ms()
+cycle_duration_ms = config.LOG_INTERVAL_SECONDS * 1000
+next_execution_delay_ms = 0
+
 while True:
-    #    utime.sleep(1)
     try:
+        current_run_time_ms = time.ticks_ms()
+        logging_platform.set_neopixel_rgb(4, 4, 0)
         mdt = machine.RTC().datetime()
-        timestamp_string = f"{mdt[0]:04d}-{mdt[1]:02d}-{mdt[2]:02d}T{mdt[4]:02d}:{mdt[5]:02d}:{mdt[6]:02d}.{mdt[7]:06d}"
-        log_data(log_file_name=get_log_file_name(), payload=timestamp_string)
-        # TODO: Replace sleep method from utime.sleep(1) to machine.lightsleep(time_ms)
-        # machine.lightsleep(850)
-        utime.sleep(1)
+        log_data(log_file_name=get_log_file_name(), payload=f'"{logging_platform.lipo_battery_gauge.get_voltage():.3f}","{logging_platform.lipo_battery_gauge.get_soc():.3f}","{current_run_time_ms}", "{next_execution_delay_ms}"')
+        logging_platform.set_neopixel_rgb(0, 0, 0)
+        next_execution_delay_ms = cycle_duration_ms - (current_run_time_ms - start_time_ms) % cycle_duration_ms
+        last_run_time_ms = current_run_time_ms
+        # time.sleep_ms(next_execution_delay_ms)
+        machine.lightsleep(next_execution_delay_ms)
     except KeyboardInterrupt:
         # Clean up and stop the timer on keyboard interrupt
         # log_timer.deinit()
